ba7eddd4241f82e99eecbe6d046f9def6c30d2f5
Aiukan <ayka.onky@gmail.com>
2025-03-28 13:30:24 +0300

    admin extension 2: add user inlines for subscription, favorite, shoppingcart

--- backend/avatar_user/admin.py
+++ backend/avatar_user/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import AvatarUser
+from shopping_cart.models import ShoppingCart
+from favorite.models import Favorite
+from subscriptions.models import Subscription
 
+
+class SubscriptionInline(admin.TabularInline):
+    model = Subscription
+    fk_name = 'user_from'
+    extra = 1
+
+
+class FavoriteInline(admin.TabularInline):
+    model = Favorite
+    extra = 1
+
+
+class ShoppingCartInline(admin.TabularInline):
+    model = ShoppingCart
+    extra = 1
 
 class CustomUserAdmin(UserAdmin):
     model = AvatarUser
@@ -10,17 +28,15 @@ class CustomUserAdmin(UserAdmin):
         'last_name', 'is_staff', 'is_active'
     )
     list_filter = ('is_staff', 'is_active')
-
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': (
+        ('Персональная информация', {'fields': ('first_name', 'last_name')}),
+        ('Права', {'fields': (
             'is_staff', 'is_active', 'is_superuser',
             'groups', 'user_permissions'
         )}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
     )
-
     add_fieldsets = (
         (
             None,
@@ -33,9 +49,10 @@ class CustomUserAdmin(UserAdmin):
             }
         ),
     )
-
     search_fields = ('email', 'username')
     ordering = ('id',)
+    list_display_links = ('id', 'username')
+    inlines = (SubscriptionInline, FavoriteInline, ShoppingCartInline)
 
 
 admin.site.register(AvatarUser, CustomUserAdmin)
